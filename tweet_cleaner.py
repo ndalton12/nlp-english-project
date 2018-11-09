1012b922145548be49761eb864b4f1bf97becffa
Niall Dalton <niall.dalton12@gmail.com>
2018-11-08 19:22:07 -0500

    Init without data

---
+++ tweet_cleaner.py
@@ -0,0 +1,17 @@
+import string
+import langid
+
+cleaned = open("data/climate_tweets_cleaned.txt", "w+")
+
+with open('data/climate_tweets.txt', 'r+') as f:
+    for line in f.readlines():
+    	if langid.classify(line)[0] != 'en':
+    		continue
+    	for word in line.split(' '):
+    		if not word:
+    			continue
+	    	if not any([x for x in ('http', '@', '#', 'RT') if x in word]) and word[0].lower() in string.ascii_lowercase:
+	    		cleaned.write('{word} '.format(word=word))
+
+cleaned.close()
+print('done')
